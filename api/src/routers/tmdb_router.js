0f13c87e52a6b389dc575b7d5bd1e007e45fae72
Joonas8s <joonas.simonson@hotmail.com>
2025-11-19 13:02:38 +0200

    Filtterit lisätty, filtterinä myös henkilöhaku

--- api/src/routers/tmdb_router.js
+++ api/src/routers/tmdb_router.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { search, details, now_playing } from "../controllers/tmdb_controller.js";
+import { search, details, now_playing, genres, search_person, person_movies } from "../controllers/tmdb_controller.js";
 
 const tmdbRouter = Router();
 
 tmdbRouter.get("/search", search);
 tmdbRouter.get("/movie/:id", details);
 tmdbRouter.get("/now_playing", now_playing);
+tmdbRouter.get("/genres", genres);
+tmdbRouter.get("/search_person", search_person);
+tmdbRouter.get("/person_movies", person_movies);
 
 export default tmdbRouter;
