47d2dc41833f0d381b4e6b2a8ca98c4e06377dea
AapoRajala <vrajala24@students.oamk.fi>
2025-12-02 13:36:28 +0200

    Groups.js päivitetty toimivaksi
    Groups.js päivitetty ja linkitetty databaseen

--- api/src/routers/groupList_router.js
+++ api/src/routers/groupList_router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createGroup, getOwnGroups } from "../controllers/groupList_controller.js";
+import { createGroup, getOwnGroups, getGroups } from "../controllers/groupList_controller.js";
 
 const groupListRouter = Router();
 
@@ -10,5 +10,7 @@ groupListRouter.post("/creategroup", createGroup);
 
 // GET own groups at "/" (requires Authorization header)
 groupListRouter.get("/getOwnGroups", getOwnGroups);
+
+groupListRouter.get("/getGroups", getGroups);
 
 export default groupListRouter;
